852466e898b76cfd809c8b144c03ced9c2513584
Wojciech Jończyk <wjonczyk@duck.com>
2022-10-27 15:08:44 +0200

    Adding Mockito test for IndexController

---
+++ src/test/java/guru/springframework/spring5recipeapp/controllers/IndexControllerTest.java
@@ -0,0 +1,37 @@
+package guru.springframework.spring5recipeapp.controllers;
+
+import guru.springframework.spring5recipeapp.services.RecipeService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.springframework.ui.Model;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class IndexControllerTest {
+
+    @Mock
+    RecipeService recipeService;
+
+    @Mock
+    Model model;
+
+    IndexController controller;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.initMocks(this);
+        controller = new IndexController(recipeService);
+    }
+
+    @Test
+    void getIndexPage() {
+        String viewName = controller.getIndexPage(model);
+        assertEquals("index", viewName);
+        Mockito.verify(recipeService, Mockito.times(1)).getRecipes();
+        Mockito.verify(model, Mockito.times(1))
+                .addAttribute(Mockito.eq("recipes"), Mockito.anySet());
+    }
+}
